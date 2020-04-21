ca0fb2194f44fe6531ca14997be3b2e02d8ca9cb
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-04-20 23:23:53 -0700

    first sw upate

--- Custom-Calculator/service-worker.js
+++ Custom-Calculator/service-worker.js
@@ -1,24 +1,47 @@
 
-const cachename = 'Custom-Calculator';
+const expectedCaches = ['Calculator-v2.0'];
 const files2cache = [
   './',
+  './manifest.json',
   './index.html',
   './css/style.css',
   './js/main.js',
-  './js/functions.js'
+  './js/functions.js',
+  './images/icons/icon-512x512.png',
+  './images/icons/icon-384x384.png',
+  './images/icons/icon-192x192.png',
+  './images/icons/icon-152x152.png',
+  './images/icons/icon-144x144.png',
+  './images/icons/icon-128x128.png',
+  './images/icons/icon-96x96.png',
+  './images/icons/icon-72x72.png',
+  './images/icons/icon-32x32.png',
+  './images/icons/icon-16x16.png'  
 ];
 
-self.addEventListener('install', function(event){
-  event.waitUntil(caches.open('static')
+self.addEventListener('install', function(event) {
+  console.log('v2.0 installing...');
+  event.waitUntil(caches.open('Calculator-v2.0')
     .then(function(cache) {
       cache.addAll(files2cache);
     }));
-  console.log('service workers installed');
+  console.log('update complete!');
 });
 
 self.addEventListener('activate', function(event){
   console.log('service workers activated');
-})
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('v2.0 ready to handle fetches!');
+    })
+  );
+});
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
