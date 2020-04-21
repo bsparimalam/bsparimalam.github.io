3209b5d1b7428929e79a9ceabeba936a1553be93
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-04-20 17:05:13 -0700

    javascript resizing

--- Custom-Calculator/js/main.js
+++ Custom-Calculator/js/main.js
@@ -2,6 +2,32 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./service-worker.js')
 	.then(function(){
-		console.log('service workers registerded')
+		console.log('service workers registerded');
 	});
-}
+};
+
+resize();
+
+function resize() {
+var windowheight = window.innerHeight;
+var windowwidth = window.innerWidth;
+var ratio = 1.5;
+
+if (windowheight < ratio*windowwidth) {
+
+	appheight = windowheight;
+	appwidth = windowheight/ratio;
+
+} else {
+
+	appheight = windowheight;
+	appwidth = windowwidth;
+
+};
+
+document.getElementsByTagName("html")[0].style.height = appheight + "px";
+document.getElementsByTagName("html")[0].style.width = appwidth + "px";
+
+console.log(windowheight, windowwidth);
+console.log(appheight, appwidth);
+};
