57ea4e370102bc02782b273e95cf8b60870ab0d7
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-04-20 19:17:38 -0700

    keyboard issue fixed"

--- Custom-Calculator/js/main.js
+++ Custom-Calculator/js/main.js
@@ -2,28 +2,43 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('./service-worker.js')
 	.then(function(){
-		console.log('service workers registerded');
+		console.log('service workers registered');
 	});
 };
 
+const screenheight = screen.availHeight;
+const screenwidth = screen.availWidth;
+
+console.log("screen: " + screen.height + "," + screen.width);
 resize();
 
 function resize() {
+
 var windowheight = window.innerHeight;
 var windowwidth = window.innerWidth;
 var ratio = 1.5;
 
-if (windowheight < ratio*windowwidth) {
+if ( screenheight > screenwidth ) {
+
+	appheight = screenheight;
+	appwidth = screenwidth;
+
+} else if ( windowheight < ratio*windowwidth) {
 
 	appheight = windowheight;
 	appwidth = windowheight/ratio;
+
+} else if ( windowheight > ratio*windowwidth) {
+
+	appwidth = windowwidth;
+	appheight = windowwidth*ratio;
 
 } else {
 
 	appheight = windowheight;
 	appwidth = windowwidth;
 
-};
+} ;
 
 document.getElementsByTagName("html")[0].style.height = appheight + "px";
 document.getElementsByTagName("html")[0].style.width = appwidth + "px";
