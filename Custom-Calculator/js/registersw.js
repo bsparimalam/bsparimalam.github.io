b0684f5834634ba7500d1505d92dc6a96c515d08
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-05-01 11:58:29 -0700

    removed duplicate

--- Custom-Calculator/js/registersw.js
+++
@@ -1,8 +0,0 @@
-
-// registering service workers
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./service-worker.js')
-	.then(function(){
-		console.log('service workers registered');
-	});
-};
