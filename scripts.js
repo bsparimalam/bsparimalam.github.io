02b3470d882b2d892128aac722e1084e32c9d5ca
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-05-26 13:42:57 -0700

    icons updated

--- scripts.js
+++ scripts.js
@@ -2,3 +2,12 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'UA-166908735-1');
+
+document.addEventListener('click', event => {
+    let target = event.target.id;
+    switch(target) {
+        case 'edge': case 'opera':
+            alert('Coming Soon!');
+            break; 
+    }
+});
