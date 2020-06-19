308c776ec52c632e15c984e037595211b97b4f4e
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-06-19 13:30:50 -0700

    .

--- minibrowser/scripts.js
+++ minibrowser/scripts.js
@@ -8,6 +8,8 @@ let ismousein;
 let link;
 let url;
 let searchengine;
+let httptype;
+
 storagename = 'minibrowser.1.0';
 userpref = JSON.parse(window.localStorage.getItem(storagename));
 let currentua = navigator.userAgent;
@@ -33,6 +35,7 @@ try {
 	}
 }
 if (isextension) {
+	httptype = 'http://';
 	chromeua = 'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36';
 	firefoxua = 'Mozilla/5.0 (Android 10; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0';
 	operaua = 'Mozilla/5.0 (Linux; Android 10; VOG-L29) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36 OPR/55.2.2719';
@@ -204,6 +207,7 @@ if (isextension) {
 } else {
 	standardplaceholder = "Type a URL";
 	searchengine = false;
+	httptype = 'https://';
 	if (userpref === null) { 
 		userpref = { 
 			'recent' : 'en.m.wikipedia.org/wiki/main_page',
@@ -311,13 +315,13 @@ function loadthepage(url) {
 	loading.style.display = 'grid';
 	url = strip(url);
 	if ((url.indexOf('.') === -1) && searchengine) {
-		browser.src = `http://${searchengine}.com/search?q=${url}`;
+		browser.src = `${httptype}${searchengine}.com/search?q=${url}`;
 		setTimeout(() => {
 			addressbox.placeholder = `${searchengine}.com`;
 			addressbox.value = '';
 		}, 500);
 	} else if (url.indexOf('.') !== -1) {
-		browser.src = 'http://' + url;
+		browser.src = `${httptype}${url}`;
 		setTimeout(() => {
 			if (url.indexOf('/') !== -1) {
 				addressbox.placeholder = url.slice(0, url.indexOf('/'));
@@ -419,5 +423,5 @@ try {
 	link = userpref.recent;
 }
 setTimeout(() => {
-	loadthepage('http://' + link);
+	loadthepage(`${httptype}${link}`);
 }, 100);
