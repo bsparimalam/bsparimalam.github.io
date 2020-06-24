b8f479e2e7f32bcb871464fcfd04664bab331d5c
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-06-24 00:03:52 -0700

    privacy policy

--- minibrowser/index.js
+++ minibrowser/index.js
@@ -420,15 +420,3 @@ try {
 setTimeout(() => {
 	loadthepage(`${httptype}${link}`);
 }, 100);
-
-// register service workers
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./install.js');
-} else {
-	console.log('serice worker not supported');
-}
-// usage metric scripts
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'UA-166908735-1');
