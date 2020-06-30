728513067aaca45bd4554dc08cd5a7066da98e2b
Bharathy S. Parimalam <bparimalam@outlook.com>
2020-06-30 10:33:21 -0700

    v.0.0.1

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 
+document.addEventListener('click', event => {
+	let elementtype = event.target.attributes[0].nodeValue;
+	console.log(elementtype);
+	if (elementtype.indexOf("screenshot") !== -1) {
+		let screenshot = event.target;
+		let container = screenshot.parentElement;
+		if (container.scrollWidth >= container.scrollLeft + screenshot.scrollWidth + 5) {
+			container.scrollLeft = container.scrollLeft + screenshot.scrollWidth;
+		} else {
+			container.scrollLeft = 0;
+		}
+	}
+});
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
