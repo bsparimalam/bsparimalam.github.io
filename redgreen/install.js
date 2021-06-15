def521b701341ad6230529eb4406d26f2d161ec2
bparimalam <bparimalam@outlook.com>
2021-06-15 09:26:29 -0700

    20210615

---
+++ redgreen/install.js
@@ -0,0 +1,45 @@
+var appversion = '0.0.2';
+const expectedCaches = ['red/green-' + appversion ];
+const files2cache = [
+    './',
+    './index.html',
+    './index.json',
+    './index.png'
+];
+
+self.addEventListener('install', function(event) {
+  console.log(appversion + ' downloading...');
+  event.waitUntil(caches.open('red/green-' + appversion )
+    .then(function(cache) {
+      cache.addAll(files2cache);
+    }));
+  console.log('app download complete!');
+});
+
+self.addEventListener('activate', function(event){
+  console.log('app installation complete!');
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('red/green-' + appversion + ' ready to load offline!');
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
